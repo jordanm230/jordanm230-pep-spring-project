4310bd2b4944116eccbf6f43669d2ad63c8004b8
jordan mayo <jordan.mayo2300@gmail.com>
2025-06-12 19:40:55 +0000

    repository and service class changes

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,30 @@
 package com.example.service;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
+@Transactional
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account addAccount(Account account) {
+        return accountRepository.save(account);
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,51 @@
 package com.example.service;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
+@Transactional
 public class MessageService {
+    MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    public Message postMessage(Message message) {
+        return messageRepository.save(message);
+    }
+
+    public Message deleteMessage(long message_id) {
+        Message message = messageRepository.findById(message_id).get();
+        messageRepository.deleteById(message_id);
+        return message;
+    }
+
+    public Message updateMessage(long message_id, String new_message) {
+        Message message = messageRepository.findById(message_id).get();
+        message.setMessageText(new_message);
+        messageRepository.save(message);
+        return message;
+    }
+
+    public Message getMessage(long message_id) {
+        return messageRepository.findById(message_id).get();
+    }
+
+    public List<Message> getUserMessages(int posted_by) {
+        return messageRepository.findByPostedBy(posted_by);
+    }
 }
